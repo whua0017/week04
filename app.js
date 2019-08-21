8ed43858f47a5bc16c69bb9b9866233a65212e23
whua0017 <whua0017@student.monash.edu>
2019-08-21 23:53:11 +1000

    add keyword

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/",function(req,res){
     res.send("Hello World");
 });
 
+app.get("/:keyword",function(req,res){
+    console.log(req.url);
+    console.log(req.params);
+});
+
 app.get("/about",function(req,res){
     res.send("This is about page");
 });
